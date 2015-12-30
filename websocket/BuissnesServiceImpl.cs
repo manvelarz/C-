5ff21988521b89f5b6688ed1e806d63711c6d2ef
manvelarz <manvelarz@gmail.com>
2015-12-30 18:08:27 +0100

    f

---
+++ websocket/BuissnesServiceImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace websocket
+{
+    class BuissnesServiceImpl:WebSocketService
+    {
+        public void onReceive(string msg) {
+            //Console.WriteLine(msg);
+
+            
+        }
+    }
+}
