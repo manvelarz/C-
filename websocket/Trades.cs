845cd7ded4b6721b8dd51608d5bba65d12c04817
manvelarz <manvelarz@gmail.com>
2015-12-30 18:06:58 +0100

    trade class

--- websocket/Trades.cs
+++ websocket/Trades.cs
@@ -13,13 +13,32 @@ namespace websocket
     public class Trades
     {
         public string channel { get; set; }
-        public List<List<string>> data { get; set; }
+        public List<List<string>> strdata { get; set; }
+       
+
+
     }
 
     public class aTrade
     {
-        public double price { get; set; }
-        public double amount { get; set; }
+        //public double price { get; set; }
+        private float _price;
+
+        public float price
+        {
+            get { return _price; }
+            set { _price = Convert.ToSingle(value) ; }
+        }
+
+        //public double amount { get; set; }
+        private float _amount;
+
+        public float amount
+        {
+            get { return _amount; }
+            set { _amount = Convert.ToSingle(value); }
+        }
+        
         public string data { get; set; }
         public string type { get; set; }
 
